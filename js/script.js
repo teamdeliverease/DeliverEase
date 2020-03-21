a93430a1081d3a057d6f700446d2ba51cd6d1cb7
Shai Bruhis <shaibruhis@gmail.com>
2020-03-21 15:15:50 -0700

    reading from firebase database

--- js/script.js
+++ js/script.js
@@ -1,7 +1,52 @@
-var map;
+var firebase = new Firebase("https://deliverease-f9eec.firebaseio.com/");
+firebase.initializeApp(config);
+
+/**
+ * Data object to be written to Firebase.
+ */
+var data = {timestamp: null, lat: null, lng: null};
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.752657, lng: -122.441311},
-    zoom: 13
+  var ourHouse = {lat: 39.7559788, lng: -122.4140809};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ourHouse,
+    zoom: 13,
+    styles: [{
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+    }, {
+      featureType: 'transit.station',
+      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+    }],
+  });
+
+  var marker = new google.maps.Marker({position: ourHouse, map: map});
+
+  var infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+    });
+  }
+
+  data.lat = pos.lat;
+  data.lng = pos.lng;
+  addToFirebase(data);
+}
+
+function addToFirebase(data) {
+  getTimestamp(function(timestamp) {
+    // Add the new timestamp to the record data.
+    data.timestamp = timestamp;
+    var ref = firebase.database().ref('volunteers').push(data, function(err) {
+      if (err) {  // Data was not written to firebase.
+        console.warn(err);
+      }
+    });
   });
 }
