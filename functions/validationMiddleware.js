6339791a0873c87e5b989683ca04828d56cdfe86
Shai Bruhis <shaibruhis@gmail.com>
2020-04-02 22:37:56 -0700

    refactored routes to reuse common code. update all user facing error messages to a generic error message. Still log to firebase even if geocode fails.

--- functions/validationMiddleware.js
+++ functions/validationMiddleware.js
@@ -1,4 +1,8 @@
 const Joi = require('@hapi/joi').extend(require('joi-phone-number'));
+
+const GENERIC_ERROR_MESSAGE =
+  'Whoops! Something went wrong, sorry about that. If this problem continues, please call us at (415) 633-6261';
+
 const validationMiddleware = (schema, property) => {
   return (req, res, next) => {
     try {
@@ -6,7 +10,7 @@ const validationMiddleware = (schema, property) => {
       return next();
     } catch (err) {
       console.error(err);
-      return res.status(400).json({ err });
+      return res.status(500).send(GENERIC_ERROR_MESSAGE);
     }
   };
 };
