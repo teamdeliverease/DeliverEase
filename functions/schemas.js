d97ef0db5aebbcf983c77322dd9c352b8a3be4bc
felixBorovikov <felixborovikov@gmail.com>
2020-04-08 21:12:16 -0700

    split first name and last name into two

--- functions/schemas.js
+++ functions/schemas.js
@@ -2,43 +2,23 @@ const Joi = require('@hapi/joi').extend(require('joi-phone-number'));
 
 const schemas = {
   requester: Joi.object().keys({
-    name: Joi.string()
-      .max(64)
-      .required(),
-    email: Joi.string()
-      .max(256)
-      .empty('')
-      .email(),
-    address: Joi.string()
-      .max(256)
-      .required(),
-    phone: Joi.string()
-      .phoneNumber({ format: 'e164' })
-      .required(),
-    termsAgreement: Joi.boolean()
-      .valid(true)
-      .required(),
-    list: Joi.string()
-      .max(1024)
-      .required(),
+    firstName: Joi.string().max(64).required(),
+    lastName: Joi.string().max(64).required(),
+    name: Joi.string().max(64).required(),
+    email: Joi.string().max(256).empty('').email(),
+    address: Joi.string().max(256).required(),
+    phone: Joi.string().phoneNumber({ format: 'e164' }).required(),
+    termsAgreement: Joi.boolean().valid(true).required(),
+    list: Joi.string().max(1024).required(),
   }),
   volunteer: Joi.object().keys({
-    name: Joi.string()
-      .max(64)
-      .required(),
-    email: Joi.string()
-      .max(256)
-      .email()
-      .required(),
-    address: Joi.string()
-      .max(256)
-      .required(),
-    phone: Joi.string()
-      .phoneNumber({ format: 'e164' })
-      .required(),
-    termsAgreement: Joi.boolean()
-      .valid(true)
-      .required(),
+    firstName: Joi.string().max(64).required(),
+    lastName: Joi.string().max(64).required(),
+    name: Joi.string().max(64).required(),
+    email: Joi.string().max(256).email().required(),
+    address: Joi.string().max(256).required(),
+    phone: Joi.string().phoneNumber({ format: 'e164' }).required(),
+    termsAgreement: Joi.boolean().valid(true).required(),
   }),
 };
 
