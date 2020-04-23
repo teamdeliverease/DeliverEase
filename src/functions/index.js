180023c7ac34d9369dc80aea80efa24833594adb
Matt Gabor <mgabor@apple.com>
2020-04-22 22:14:53 -0700

    added next.js, modified folder structure

---
+++ src/functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const expressServer = require('./expressServer');
+const admin = require('firebase-admin');
+const next = require('next');
+
+admin.initializeApp();
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, conf: { distDir: 'next' } });
+const handle = app.getRequestHandler();
+
+exports.next = functions.https.onRequest((req, res) => {
+  console.log('File: ' + req.originalUrl); // log the page.js file that is being requested
+  return app.prepare().then(() => handle(req, res));
+});
+
+exports.dbTriggers = require('./dbTriggers');
+exports.app = functions.https.onRequest(expressServer);
