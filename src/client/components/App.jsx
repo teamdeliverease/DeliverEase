7520114cd7981a18ecc7c4e257796bd09a1a90e0
Matt Gabor <mgabor@apple.com>
2020-04-25 15:14:46 -0700

    added react bootstrap, theme scss

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,8 +1,10 @@
+import Button from 'react-bootstrap/Button';
 import Header from './Header';
 
 const App = ({ children }) => (
   <main>
     <Header />
+    <Button onClick={() => console.log('clicked')}>Click me</Button>
     {children}
   </main>
 );
