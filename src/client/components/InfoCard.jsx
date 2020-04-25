fdcb7be430dd35f14a1f8feefc537cc097c73d38
felixBorovikov <felixborovikov@gmail.com>
2020-04-25 15:34:51 -0700

    first pass Info Card

---
+++ src/client/components/InfoCard.jsx
@@ -0,0 +1,16 @@
+function InfoCard({ image, title, text }) {
+  return (
+    <div className="item col-12 col-md-6 col-lg-6">
+      <div className="item-inner p-3 p-lg-4">
+        <div className="item-header mb-3">
+          <div className="item-icon mb-3">
+            <img className="img-fluid" src={image} height="50" width="65" alt="notepad" />
+          </div>
+          <h3 className="item-heading"> {title} </h3>
+        </div>
+        <div className="item-desc"> {text} </div>
+      </div>
+    </div>
+  );
+}
+export default InfoCard;
--- src/client/pages/index.jsx
+++ src/client/pages/index.jsx
@@ -1,7 +1,17 @@
 import App from '../components/App';
+import InfoCard from '../components/InfoCard';
 
 export default () => (
   <App>
     <p>Index Page</p>
+    <div>
+      <InfoCard
+        image="/assets/images/piggyBank.png"
+        title=" Zero fees "
+        text="No service or delivery fees!
+        That means $0. Payments for groceries and purchased goods will be arranged between the
+        volunteer and community members."
+      />
+    </div>
   </App>
 );
