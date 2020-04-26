be209e969b67688b000cbb21ca15a78bb8f19265
Danny Eliahu <dseliahu@gmail.com>
2020-04-26 00:52:07 -0700

    Add new section to index.jsx

--- src/client/pages/index.jsx
+++ src/client/pages/index.jsx
@@ -2,12 +2,14 @@ import App from '../components/App';
 import WelcomeSection from '../components/IndexSections/WelcomeSection';
 import HowItWorksSection from '../components/IndexSections/HowItWorksSection';
 import AboutSection from '../components/IndexSections/AboutSection';
+import SubmitRequestSection from '../components/IndexSections/SubmitRequestSection';
 
 export default () => (
   <App>
     <p>Index Page</p>
     <WelcomeSection />
     <HowItWorksSection />
+    <SubmitRequestSection />
     <AboutSection />
   </App>
 );
