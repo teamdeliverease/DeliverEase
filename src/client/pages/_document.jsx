306fc8ec07c99b163e65c86e84b8ba8cd055efa0
Matt Gabor <mgabor@apple.com>
2020-04-26 16:53:55 -0700

    added facebook pixel, added title and meta tags, added font

--- src/client/pages/_document.jsx
+++ src/client/pages/_document.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash/object';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import FacebookPixel from '../components/FacebookPixel';
 
 class CustomDocument extends Document {
   render() {
@@ -28,6 +29,29 @@ class CustomDocument extends Document {
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossOrigin="anonymous"
           />
+          {/* google font */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Quicksand:700|Roboto:400,400i,700&display=swap"
+            rel="stylesheet"
+          />
+          <title>DeliverEase</title>
+          <meta charset="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="description"
+            content="DeliverEase - helping connect local volunteers with community members in need"
+          />
+          <meta property="og:title" content="DeliverEase" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="assets/images/shareThumbnail.png" />
+          <meta
+            property="og:description"
+            content="Helping connect local volunteers with community members in need."
+          />
+          <meta property="og:url" content="https://teamdeliverease.com/" />
+          <link rel="shortcut icon" href="favicon.ico" />
+          <FacebookPixel />
         </Head>
         <body>
           <Main />
