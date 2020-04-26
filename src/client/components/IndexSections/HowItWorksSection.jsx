923635d8e74b8ffec1247bb16352e0c8f10b60be
felixBorovikov <felixborovikov@gmail.com>
2020-04-26 01:18:51 -0700

    How It Works section with Info Cards

--- src/client/components/IndexSections/HowItWorksSection.jsx
+++ src/client/components/IndexSections/HowItWorksSection.jsx
@@ -2,15 +2,43 @@ import InfoCard from '../InfoCard';
 
 const HowItWorksSection = () => {
   return (
-    <div>
-      <InfoCard
-        image="/assets/images/piggyBank.png"
-        title=" Zero fees "
-        text="No service or delivery fees!
-  That means $0. Payments for groceries and purchased goods will be arranged between the
-  volunteer and community members."
-      />
-    </div>
+    <section id="benefits-section" className="benefits-section theme-bg-light-gradient py-5">
+      <div className="container">
+        <h2 className="section-heading text-center">How It Works</h2>
+        <div className="section-intro single-col-max mx-auto text-center mb-3">
+          Turning the quarantine into a quaranteam
+        </div>
+        <div className="row text-center">
+          <InfoCard
+            image="/assets/images/lightbulb.png"
+            title=" Volunteers sign up to make deliveries "
+            text="Volunteers rock! They’ll fulfill requests whenever they were originally planning on
+            going to a store themselves, making it convenient to pick up requested items."
+          />
+          <InfoCard
+            image="/assets/images/notepad.png"
+            title=" Anyone who could use help can submit a request below "
+            text="Whether you have a particular vulnerability or are a healthcare practitioner
+            intentionally staying away from public areas, we’re here to help."
+          />
+        </div>
+        <div className="row text-center">
+          <InfoCard
+            image="/assets/images/heart.png"
+            title=" We’ll pass requests to volunteers in your neighborhood "
+            text="They’ll let us know when they can fulfill your request, and we’ll let you know when
+            to expect your delivery."
+          />
+          <InfoCard
+            image="/assets/images/piggyBank.png"
+            title=" Zero fees "
+            text="No service or delivery fees!
+            That means $0. Payments for groceries and purchased goods will be arranged between the
+            volunteer and community members."
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
