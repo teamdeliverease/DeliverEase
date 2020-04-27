d33fffeb06bc59d598867cc81a79e703d2e97fcb
Matt Gabor <mgabor@apple.com>
2020-04-26 18:24:19 -0700

    added initialzeApp local

--- src/client/components/MetaTags.jsx
+++ src/client/components/MetaTags.jsx
@@ -2,7 +2,7 @@
 
 const MetaTags = () => (
   <>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta
--- src/client/utils/auth/initFirebase.js
+++ src/client/utils/auth/initFirebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
+const localConfig = {
   apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
@@ -9,7 +9,12 @@ const config = {
 };
 
 export default () => {
+  const dev = process.env.NODE_ENV !== 'production';
   if (!firebase.apps.length) {
-    firebase.initializeApp(config);
+    if (dev) {
+      firebase.initializeApp(localConfig);
+    } else {
+      firebase.initializeApp();
+    }
   }
 };
