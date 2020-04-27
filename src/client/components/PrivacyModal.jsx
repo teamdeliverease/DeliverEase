797eec6578f5df5611653da7dcf222b9f02a2242
felixBorovikov <felixborovikov@gmail.com>
2020-04-26 17:13:53 -0700

    About Section and Privacy Model

---
+++ src/client/components/PrivacyModal.jsx
@@ -0,0 +1,31 @@
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+const PrivacyModal = ({ title, body, button }) => {
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {button}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{body}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default PrivacyModal;
--- src/client/pages/index.jsx
+++ src/client/pages/index.jsx
@@ -1,11 +1,13 @@
 import App from '../components/App';
 import WelcomeSection from '../components/IndexSections/WelcomeSection';
 import HowItWorksSection from '../components/IndexSections/HowItWorksSection';
+import AboutSection from '../components/IndexSections/AboutSection';
 
 export default () => (
   <App>
     <p>Index Page</p>
     <WelcomeSection />
     <HowItWorksSection />
+    <AboutSection />
   </App>
 );
