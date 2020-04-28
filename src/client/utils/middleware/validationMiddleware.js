06a3ec063f24ff3d79428d6c189a7aa2315f5f10
Shai Bruhis <shaibruhis@gmail.com>
2020-04-28 01:15:35 -0700

    rewrote validation middleware to take params in directly and not wrap the whole handler

--- src/client/utils/middleware/validationMiddleware.js
+++ src/client/utils/middleware/validationMiddleware.js
@@ -2,14 +2,11 @@ import { GENERIC_ERROR_MESSAGE } from '../../constants';
 
 const Joi = require('@hapi/joi').extend(require('joi-phone-number'));
 
-export default (handler, schema) => {
-  return (req, res) => {
-    try {
-      Joi.assert(req.body, schema);
-      return handler(req, res);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send(GENERIC_ERROR_MESSAGE);
-    }
-  };
+export default (req, res, schema) => {
+  try {
+    return Joi.assert(req.body, schema);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send(GENERIC_ERROR_MESSAGE);
+  }
 };
