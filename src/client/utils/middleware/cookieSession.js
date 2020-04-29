7c0deaed527baf5feeb47817e2bb4280b37b9a83
Matt Gabor <mattgabor@gmail.com>
2020-04-28 18:36:27 -0700

    fix session handling routes (#153)
    Co-authored-by: Matt Gabor <mgabor@apple.com>

--- src/client/utils/middleware/cookieSession.js
+++ src/client/utils/middleware/cookieSession.js
@@ -2,21 +2,19 @@ import cookieSession from 'cookie-session';
 import * as functions from 'firebase-functions';
 
 export const addSession = (req, res) => {
+  const SESSION_SECRET_CURRENT = functions.config().auth.session_secret_current;
+  const SESSION_SECRET_PREVIOUS = functions.config().auth.session_secret_current;
   // Ensure that session secrets are set.
-  if (!(process.env.SESSION_SECRET_CURRENT && process.env.SESSION_SECRET_PREVIOUS)) {
+  if (!(SESSION_SECRET_CURRENT && SESSION_SECRET_PREVIOUS)) {
     throw new Error(
-      'Session secrets must be set as env vars `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.',
+      'Session secrets must be set from firebase config `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.',
     );
   }
 
   // An array is useful for rotating secrets without invalidating old sessions.
   // The first will be used to sign cookies, and the rest to validate them.
   // https://github.com/expressjs/cookie-session#keys
-  // const sessionSecrets = [process.env.SESSION_SECRET_CURRENT, process.env.SESSION_SECRET_PREVIOUS];
-  const sessionSecrets = [
-    functions.config().auth.session_secret_current,
-    functions.config().auth.session_secret_previous,
-  ];
+  const sessionSecrets = [SESSION_SECRET_CURRENT, SESSION_SECRET_PREVIOUS];
 
   // Example:
   // https://github.com/billymoon/micro-cookie-session
