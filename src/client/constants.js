215099fd3574587583ece2eb016994647fb30855
Shai Bruhis <shaibruhis@gmail.com>
2020-05-06 00:02:08 -0700

    migrated post requests from route to client side

--- src/client/constants.js
+++ src/client/constants.js
@@ -9,7 +9,6 @@ export const FULFILLMENT_STATUS = {
 export const GENERIC_ERROR_MESSAGE =
   'Whoops! Something went wrong, sorry about that. If this problem continues, please call us at (415) 633-6261';
 
-export const REQUESTERS_REF = 'requesters';
 export const VOLUNTEERS_REF = 'volunteers';
 export const MAPS_API_KEY = 'AIzaSyBnw4vCWKTZgb-OMJPJ15ptB4dEa5zxQnQ';
 
