d33769859ef00ca575f476939c2b9ed161358696
Matt Gabor <mgabor@apple.com>
2020-05-09 11:11:33 -0700

    refactor form submission to happen within forms

--- src/client/components/IndexSections/RequestForm.jsx
+++ src/client/components/IndexSections/RequestForm.jsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from 'formik';
-
+import { GENERIC_ERROR_MESSAGE } from '../../constants';
 import TextInput from '../FormPrimitives/TextInput';
 import PhoneNumberInput from '../FormPrimitives/PhoneNumberInput';
 import AddressInput from '../FormPrimitives/AddressInput';
@@ -7,6 +7,7 @@ import SelectInput from '../FormPrimitives/SelectInput';
 import CheckBoxInput from '../FormPrimitives/CheckBoxInput';
 import Modal from '../Modal';
 import RequestTermsOfUse from '../FormPrimitives/RequestTermsOfUse';
+import { postRequester } from '../../api/requesters';
 
 const languageOptions = [
   { value: 'English', label: 'English' },
@@ -16,7 +17,27 @@ const languageOptions = [
   { value: 'Other', label: 'Other' },
 ];
 
-const RequestForm = ({ onSubmit, submitDisabled }) => {
+const RequestForm = ({ onSubmitted }) => {
+  const handleSubmit = async (formData, setSubmitting) => {
+    try {
+      // disable submit button while waiting on api call
+      setSubmitting(true);
+      // TODO: change this to just take in formData when forms are properly hooked up
+      await postRequester({
+        ...formData,
+        phone: '+19162061598',
+        termsAgreement: true,
+      });
+
+      onSubmitted();
+      // analytics.logEvent('sign_up', { method: 'volunteer' });
+    } catch (err) {
+      console.error(err);
+      setSubmitting(false);
+      alert(GENERIC_ERROR_MESSAGE);
+    }
+  };
+
   return (
     <Formik
       initialValues={{
@@ -29,55 +50,55 @@ const RequestForm = ({ onSubmit, submitDisabled }) => {
         language: '',
       }}
       onSubmit={(values, { setSubmitting }) => {
-        console.log(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-        onSubmit(values);
+        handleSubmit(values, setSubmitting);
       }}
     >
-      <div className="form-wrapper">
-        <Form className="text-center flex-wrap px-lg-5">
-          <div className="form-row">
-            <TextInput colWidth="6" maxLength="64" name="firstName" placeholder="First Name" />
-            <TextInput colWidth="6" maxLength="64" name="lastName" placeholder="Last Name" />
-          </div>
-          <PhoneNumberInput name="phone" placeholder="Phone Number" />
-          <AddressInput name="address" id="requesterAddress" placeholder="Street Address" />
-          <TextInput
-            maxLength="256"
-            isRequired={false}
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-          <SelectInput name="language" options={languageOptions} placeholder="Languages Spoken" />
-          <TextInput
-            inputType="long"
-            maxLength="1024"
-            rows="4"
-            name="list"
-            placeholder="Enter shopping list here"
-          />
-          <CheckBoxInput name="termsOfUse">
-            <>
-              I agree to the{' '}
-              <Modal
-                title="Requester Terms and Conditions"
-                linkText="Terms of Use"
-                linkColor="blue"
-              >
-                <RequestTermsOfUse />
-              </Modal>
-            </>
-          </CheckBoxInput>
-          <button
-            disabled={submitDisabled}
-            className="btn btn-primary btn-submit mx-auto"
-            type="submit"
-          >
-            Submit Request
-          </button>
-        </Form>
-      </div>
+      {(props) => (
+        <div className="form-wrapper">
+          <Form className="text-center flex-wrap px-lg-5">
+            <div className="form-row">
+              <TextInput colWidth="6" maxLength="64" name="firstName" placeholder="First Name" />
+              <TextInput colWidth="6" maxLength="64" name="lastName" placeholder="Last Name" />
+            </div>
+            <PhoneNumberInput name="phone" placeholder="Phone Number" />
+            <AddressInput name="address" id="requesterAddress" placeholder="Street Address" />
+            <TextInput
+              maxLength="256"
+              isRequired={false}
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <SelectInput name="language" options={languageOptions} placeholder="Languages Spoken" />
+            <TextInput
+              inputType="long"
+              maxLength="1024"
+              rows="4"
+              name="list"
+              placeholder="Enter shopping list here"
+            />
+            <CheckBoxInput name="termsOfUse">
+              <>
+                I agree to the{' '}
+                <Modal
+                  title="Requester Terms and Conditions"
+                  linkText="Terms of Use"
+                  linkColor="blue"
+                >
+                  <RequestTermsOfUse />
+                </Modal>
+              </>
+            </CheckBoxInput>
+            <button
+              disabled={props.isSubmitting}
+              className="btn btn-primary btn-submit mx-auto"
+              type="submit"
+            >
+              Submit Request
+            </button>
+          </Form>
+        </div>
+      )}
     </Formik>
   );
 };
--- src/client/components/IndexSections/SubmitRequestSection.jsx
+++ src/client/components/IndexSections/SubmitRequestSection.jsx
@@ -4,34 +4,11 @@ import ShareCard from '../ShareCard';
 import {
   REQUESTER_SHARE_CONTENT as shareContent,
   REQUESTER_SHARE_MESSAGE as shareMessage,
-  GENERIC_ERROR_MESSAGE,
 } from '../../constants';
 import 'firebase/analytics';
-import { postRequester } from '../../api/requesters';
 
 const SubmitRequestSection = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleSubmit = async (formData) => {
-    try {
-      // disable submit button while waiting on api call
-      setSubmitting(true);
-      // TODO: change this to just take in formData when forms are properly hooked up
-      await postRequester({
-        ...formData,
-        phone: '+19162061598',
-        termsAgreement: true,
-      });
-
-      setSubmitted(true);
-      // analytics.logEvent('sign_up', { method: 'requester' });
-    } catch (err) {
-      console.error(err);
-      setSubmitting(false);
-      alert(GENERIC_ERROR_MESSAGE);
-    }
-  };
+  const [hasSubmitted, setSubmitted] = useState(false);
 
   return (
     <section id="request-section" className="request-section">
@@ -45,7 +22,7 @@ const SubmitRequestSection = () => {
                   <h2 className="form-heading text-center" id="request-title">
                     DeliverEase will find you a volunteer in your neighborhood
                   </h2>
-                  {submitted ? (
+                  {hasSubmitted ? (
                     <ShareCard message={shareMessage} content={shareContent} />
                   ) : (
                     <>
@@ -53,7 +30,7 @@ const SubmitRequestSection = () => {
                         You&apos;ll get notified once we find a match for your request and a
                         volunteer will drop off your items directly to your front door.
                       </div>
-                      <RequestForm onSubmit={handleSubmit} submitDisabled={submitting} />
+                      <RequestForm onSubmitted={() => setSubmitted(true)} />
                       <div className="text-left small mt-4">
                         By clicking &quot;Submit Request&quot;, you consent to recieving text
                         messages and phone calls from the DeliverEase team and its volunteers.
