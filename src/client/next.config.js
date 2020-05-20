b5a491cf380ba8df5dfe74c995b711f4e11111c1
Matt Gabor <mgabor@apple.com>
2020-05-19 21:11:28 -0700

    got deployment by environment working

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,15 +1,15 @@
-// require('../../env.js');
+require('../../env.js');
 
 module.exports = {
   distDir: '../../dist/client',
   // Public, build-time env vars.
   // https://nextjs.org/docs#build-time-configuration
   env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_PUBLIC_API_KEY: process.env.FIREBASE_PUBLIC_API_KEY,
     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
 };
